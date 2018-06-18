c8e65f1992f5d5b0240f66b2ff04e0c389fee563
Amit Chaudhari <achaudhari@guidewire.com>
2018-06-18 08:49:59 -0700

    Adding Imdb sourcecode

---
+++ src/BaseConfiguration.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public interface BaseConfiguration {
+
+    String BASE_URL = "https://www.imdb.com";
+    String MOVIE_URL_250 = "/chart/top";
+    String RESULTS = "./Results.csv";
+    int CRWAL_LIMIT = 250;
+    HtmlUnitDriver driver = new HtmlUnitDriver();
+
+}
---
+++ src/Locators.java
@@ -0,0 +1,8 @@
+public class Locators {
+
+    public static final String top250ListLocator = "table[class^=\"chart\"] tr td[class^=\"title\"] a";
+    public static final String top250RatingLocator = "table[class^=\"chart\"] tr td[class^=\"rating\"] strong";
+    public static final String genreMovieTitleLocator = "div[class^='lister-item mode'] h3 a";
+    public static final String genreMovieRatingLocator = "div[class^='ratings'] strong";
+
+}
